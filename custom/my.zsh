a8f17a476f95bb4e6b46dce94357def697f00de6
Boaz Yaniv <boazy@agatsolutions.com>
2013-05-09 03:02:08 +0300

    Commit

---
+++ custom/my.zsh
@@ -0,0 +1,38 @@
+# For kcd
+KCD_INIT="/etc/profile.d/kcd.sh"
+if [ -e "$KCD_INIT" ]; then
+	. "$KCD_INIT"
+fi
+
+# z (Jump directories)
+source ~/.zsh/ext/z/z.sh
+alias zc='z -c'
+
+# Aliases
+alias ll='ls --color=auto -al --group-directories-first'
+alias md='mkdir'
+
+lm() {
+  ls --color -al --group-directories-first $* | less -R
+}
+
+agenv () {
+  source ~/proj/agenv
+}
+
+agld () {
+  export LD_LIBRARY_PATH=/opt/pkg/agat/lib
+}
+
+fancy-ctrl-z () {
+  if [[ $#BUFFER -eq 0 ]]
+  then
+    bg
+  zle redisplay
+  else
+    zle push-input
+  fi
+}
+
+zle -N fancy-ctrl-z
+bindkey '^Z' fancy-ctrl-z
